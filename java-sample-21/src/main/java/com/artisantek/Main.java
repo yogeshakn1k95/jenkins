7432a8b8745efd2c1d3135e16e014284040694cf
adithyagarapati <adithyavardhan93@gmail.com>
2025-06-25 17:21:41 +0530

    Removed Spring boot

--- java-sample-21/src/main/java/com/artisantek/Main.java
+++ java-sample-21/src/main/java/com/artisantek/Main.java
@@ -6,14 +6,12 @@ import io.javalin.http.staticfiles.Location;
 public class Main {
     public static void main(String[] args) {
         Javalin app = Javalin.create(config -> {
-            config.staticFiles.add("/public", Location.CLASSPATH);
+            // This will serve "index.html" from "resources/public" on the root path "/"
+            // It's the most efficient way to serve a single-page or static site root.
+            config.spaRoot.addFile("/", "/public/index.html", Location.CLASSPATH);
         }).start(5000);
 
         System.out.println("\nApplication is running on http://localhost:5000");
         System.out.println("Press Ctrl+C to stop.");
-
-        app.get("/", ctx -> {
-            ctx.html(Main.class.getResourceAsStream("/public/index.html"));
-        });
     }
 } 
